02813338052bc8b83b9da64f877c8df294ba034d
itggot-william-holmberg <erikholmberg17@Eriks-MacBook-Pro-3.local>
2016-11-25 19:04:06 +0100

    dam

--- src/data/agility/GnomeData.java
+++ src/data/agility/GnomeData.java
@@ -7,6 +7,8 @@ import enums.assignment.agility.AgilityObstacle;
 
 public class GnomeData {
 	
+	public static Area GNOME_GATE_AREA = new Area(2459,3384,2463,3381);
+	
 	private static Area AREA_1_BALANCE_LOG_AREA = new Area(2477,3436,2472,3439);
 	
 	private static Area AREA_2_CLIMB_NET = new Area(2477,3429,2470,3426);
@@ -16,5 +18,5 @@ public class GnomeData {
 												 new AgilityObstacle(AREA_2_CLIMB_NET, "Climb-over", 23134)};
 	
 	public static Area WHOLE_COURSE_AREA = new Area(new Tile[] { new Tile(2472,3441,0), new Tile(2478,3441,0), new Tile(2468,3437,0), new Tile(2468,3434,0), new Tile(2468,3425,0), new Tile(2468,3421,0), new Tile(2468,3417,0), new Tile(2470,3415,0), new Tile(2473,3413,0), new Tile(2475,3413,0), new Tile(2479,3413,0), new Tile(2483,3413,0), new Tile(2486,3413,0), new Tile(2487,3413,0), new Tile(2490,3416,0), new Tile(2491,3418,0), new Tile(2491,3421,0), new Tile(2491,3425,0), new Tile(2491,3429,0), new Tile(2491,3432,0), new Tile(2491,3436,0), new Tile(2489,3439,0), new Tile(2487,3441,0), new Tile(2481,3441,0), new Tile(2474,3441,0), new Tile(2471,3440,0),});
-
+	
 }
--- src/state/agility/WalkToAgilityCourse.java
+++ src/state/agility/WalkToAgilityCourse.java
@@ -1,6 +1,7 @@
 package state.agility;
 
 import org.tbot.internal.handlers.LogHandler;
+import org.tbot.methods.NPCChat;
 import org.tbot.methods.Players;
 import org.tbot.methods.walking.Walking;
 
@@ -25,14 +26,16 @@ public class WalkToAgilityCourse extends State {
 			WalkToGnomeCourse();
 			break;
 		}
-		Data.currentMission = "Lets go to the agility course";
-		Walking.findPath(AgilityResources.gnomeAgilityArea.getCentralTile()).traverse();
+		//Data.currentMission = "Lets go to the agility course";
+		//Walking.findPath(AgilityResources.gnomeAgilityArea.getCentralTile()).traverse();
 		
 	}
 
 	private void WalkToGnomeCourse() {
-		if(GnomeData.GATE_AREA.contains(Players.getLocal())){
-			//open gate
+		if(GnomeData.GNOME_GATE_AREA.contains(Players.getLocal())){
+			if(NPCChat.getNPCChatWidget() != null){
+				LogHandler.log("tjena!");
+			}
 		} //else if() player should tele		
 		else{
 			Walking.findPath(GnomeData.OBSTACLES[0].getArea().getCentralTile()).traverse();
